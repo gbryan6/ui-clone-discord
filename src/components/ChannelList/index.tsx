82ce629f595359de8c7f157074904183d6e7580e
Gabriel Bryan <gbryan6@hotmail.com>
2021-08-19 00:35:37 -0300

    feat: make a channel component and buttons

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+import ChannelButton from '../ChannelButton'
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+        <Category>
+            <span>Canais de texto</span>
+            <AddCategoryIcon />
+        </Category>
+
+        <ChannelButton  channelName="chat-livre" />
+        <ChannelButton  channelName="html" />
+        <ChannelButton  channelName="css" />
+        <ChannelButton  channelName="reactjs" />
+        <ChannelButton  channelName="trabalho" />
+
+    </Container>
+  );
+}
+
+export default ChannelList;
